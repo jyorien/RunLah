1186225b7feda66fde99ae0e85856f9700536865
joey <jyorin0206@gmail.com>
2021-04-29 23:15:54 +0800

    detects when screen is upside down

--- app/src/main/java/com/example/runlah/home/MainActivity.kt
+++ app/src/main/java/com/example/runlah/home/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.runlah.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.SensorManager
+import android.hardware.display.DisplayManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.MenuItem
+import android.view.OrientationEventListener
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.runlah.R
 import com.example.runlah.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -17,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navigateToFragment()
+        val orientationEventListener = object : OrientationEventListener(this, SensorManager.SENSOR_DELAY_NORMAL) {
+            override fun onOrientationChanged(orientation: Int) {
+                if (orientation in 151..199)
+                    Log.i("hello","upside down")
+            }
+        }
+        orientationEventListener.enable()
     }
 
     fun navigateToFragment() {
